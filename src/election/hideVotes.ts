f28c9869f78d410c2b092c4397acf965c347c0b2
David3Emmanuel <david3emmanuel@gmail.com>
2025-01-08 23:35:37 +0100

    add vote count and year to fetch response

--- src/election/hideVotes.ts
+++ src/election/hideVotes.ts
@@ -1,8 +1,17 @@
-import { ElectionWithId } from 'src/schemas/election.schema'
+import { ApiProperty } from '@nestjs/swagger'
+import { ElectionWithId, getYear } from 'src/schemas/election.schema'
 
 export function hideVotes(election: ElectionWithId): ElectionWithoutVotes {
-  return { ...election, votes: undefined }
+  return {
+    ...election,
+    votes: undefined,
+    voteCount: election.votes?.length ?? 0,
+    year: getYear(election),
+  }
 }
+
 export class ElectionWithoutVotes extends ElectionWithId {
   votes: undefined
+  @ApiProperty() voteCount: number
+  @ApiProperty() year: number
 }
