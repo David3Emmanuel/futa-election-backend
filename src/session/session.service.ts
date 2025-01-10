d6f5881ba3a145f232f7a46be9eec5e5ebe2bec3
David3Emmanuel <david3emmanuel@gmail.com>
2025-01-10 14:53:55 +0100

    feat: implement session limit and remove stale session

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { extractSession, Session } from 'src/schemas/session.schema'
@@ -6,7 +7,12 @@ import { extractSession, Session } from 'src/schemas/session.schema'
 @Injectable()
 export class SessionService {
   started = false
-  constructor(@InjectModel(Session.name) private model: Model<Session>) {
+  maxSessions = 2
+
+  constructor(
+    @InjectModel(Session.name) private model: Model<Session>,
+    private jwtService: JwtService,
+  ) {
     // Delete all sessions
     console.log('Resetting sessions...')
     this.model
@@ -24,23 +30,52 @@ export class SessionService {
     }
   }
 
+  private async canCreateWithinLimits(userId: string) {
+    const count = await this.model.countDocuments({ userId }).exec()
+    if (count >= this.maxSessions) await this.removeStaleSessions(userId)
+    else return true
+
+    const recount = await this.model.countDocuments({ userId }).exec()
+    return recount < this.maxSessions
+  }
+
+  private async removeStaleSessions(userId: string) {
+    const sessions = (await this.model.find({ userId }).exec()).map(
+      extractSession,
+    )
+    const toDelete = sessions
+      .filter((session) => {
+        try {
+          this.jwtService.verify(session.accessToken)
+          return false
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (e) {
+          return true
+        }
+      })
+      .map((session) => session._id)
+
+    if (toDelete.length > 0)
+      await this.model.deleteMany({ _id: { $in: toDelete } }).exec()
+  }
+
   async createSession(userId: string, accessToken: string) {
     await this.waitForStart()
+    if (!(await this.canCreateWithinLimits(userId)))
+      throw new ForbiddenException()
+
     const created = await new this.model({ userId, accessToken }).save()
-    console.log(await this.model.find().exec())
     return extractSession(created)
   }
 
   async findSession(userId: string, accessToken: string) {
     await this.waitForStart()
     const session = await this.model.findOne({ userId, accessToken }).exec()
-    console.log(await this.model.find().exec())
     return session && extractSession(session)
   }
 
   async deleteSessionsByUserId(userId: string) {
     await this.waitForStart()
     await this.model.deleteMany({ userId }).exec()
-    console.log(await this.model.find().exec())
   }
 }
