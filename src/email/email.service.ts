137c3095e06b092e5a062a165e60bc2d8d913386
David3Emmanuel <david3emmanuel@gmail.com>
2025-01-17 07:43:08 +0100

    feat: implement retry logic for email sending in EmailService

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -14,19 +14,27 @@ export class EmailService {
   }
 
   async sendMail(to: string, templateId: number, params: object) {
-    const res = await fetch('https://api.brevo.com/v3/smtp/email', {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'api-key': this.authorization,
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        params,
-        templateId,
-        to: [{ email: to }],
-      }),
-    })
+    let res
+    while (!res) {
+      try {
+        res = await fetch('https://api.brevo.com/v3/smtp/email', {
+          method: 'POST',
+          headers: {
+            accept: 'application/json',
+            'api-key': this.authorization,
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            params,
+            templateId,
+            to: [{ email: to }],
+          }),
+        })
+      } catch (e) {
+        console.error(e)
+        console.warn('Retrying...')
+      }
+    }
     if (res.ok) return await res.json()
     else {
       try {
